e64bef9ca1e0d99f03abcc8b1aa42450c088f657
warrior <abdoulaye.kane093@gmail.com>
2020-01-09 06:56:31 +0000

    Adding new component header and footer

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
